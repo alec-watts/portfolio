169358ac9e8d1b12126ec2b49d085adc71529402
Alexander Watts <alecwatts1@gmail.com>
2025-01-31 10:22:02 -0800

    face

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,34 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 import Image from "next/image";
 import { Link as LinkIcon } from "lucide-react";
 import Link from "next/link";
+import Face from "@/public/face.jpeg";
+import LinkedIn from "@/public/linkedin.png";
+import Github from "@/public/github.png";
 
 export default function Home() {
   return (
     <div className="w-full md:w-2/3 lg:w-1/2 flex flex-col gap-2">
-      <h1 className="text-3xl font-bold">Education</h1>
-      {education.map((education) => (
+      {/* Header */}
+      <div className="flex gap-2 pt-10">
+        <Image src={Face} alt="Face" className="w-36 h-auto rounded-lg" />
+
+        <div className="flex flex-col gap-1">
+          <h1 className="text-3xl font-bold">Alexander Watts</h1>
+          <h2 className="text-xl font-bold">Software Engineer</h2>
+
+          <div className="flex gap-2">
+            <Link href="https://www.linkedin.com/in/alecwatts" target="_blank">
+              <Image src={LinkedIn} alt="LinkedIn" className="w-6 h-auto" />
+            </Link>
+            <Link href="https://github.com/alecwatts" target="_blank">
+              <Image src={Github} alt="Github" className="w-6 h-auto" />
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <h1 className="text-3xl font-semibold pt-10">Education</h1>
+      {education.map((education)   => (
         <Card key={education.school}>
           <CardHeader className="flex flex-row gap-2 p-4 pb-2">
             <Image src={education.image} alt={education.school} className="h-10 w-auto" />
@@ -28,7 +50,7 @@ export default function Home() {
         </Card>
       ))}
 
-      <h1 className="text-3xl font-bold">My Work</h1>
+      <h1 className="text-3xl font-semibold pt-10">My Work</h1>
       {work.map((work) => (
         <Card key={work.title}>
           <CardHeader className="pb-3">  
